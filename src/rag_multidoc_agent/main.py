b58f56f72ef306db0452a4e3f8c885cab1bfc0c6
raistln <samumarfon@gmail.com>
2025-04-15 15:56:32 +0200

    first commit

---
+++ src/rag_multidoc_agent/main.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from typing import List, Dict, Any
+
+def main():
+    """
+    Main function to run the application.
+    """
+    print("Hello from main.py!")
+    
+if __name__ == "__main__":
+    main() 
